f80046c99219a26b05e7372892f5c10f258ff33a
k3rlll <abdyashimramazan@gmail.com>
2025-10-13 00:19:06 +0500

    ss

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -18,6 +18,11 @@ func main() {
 
 	log := setupLogger(cfg.Env)
 
+	log.Info("startin application",
+		slog.String("env", cfg.Env),
+		slog.String("storage_path", cfg.StoragePath),
+		slog.String("", cfg.GRPC.Timeout.String()))
+
 }
 
 func setupLogger(env string) *slog.Logger {
@@ -29,7 +34,7 @@ func setupLogger(env string) *slog.Logger {
 	case envDev:
 		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
 	case envProd:
-		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}))
+		log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}))
 
 	}
 	return log
